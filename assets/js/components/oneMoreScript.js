6cbff64e8ee7764c4f1c07a2ca376a203ba076a8
Maxopenstudio <71130450+Maxopenstudio@users.noreply.github.com>
2024-03-11 15:06:37 +0200

    reviews + tariff

--- assets/js/components/oneMoreScript.js
+++ assets/js/components/oneMoreScript.js
@@ -1,9 +1,41 @@
-$(".burger").click(function() {
-    $(".nav").addClass("open");
+$(document).ready(function() {
+    $(".burger").click(function() {
+        $(".nav").addClass("open");
+    });
+    $(".close").click(function() {
+        $(".nav").removeClass("open");
+    });
+
+    $('.reviews__slider').owlCarousel({
+        loop:true,
+        nav:true,
+        mouseDrag:true,
+        touchDrag: true,
+        pullDrag:false,
+        freeDrag:false,
+        URLhashListener:false,
+        dots:true,
+        autoplay:false,
+        margin: 24,
+        center:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            576:{
+                items:2
+            },
+            768:{
+                items:3
+            }
+        }
+    });
+
+    $(".tariff__options-select").click(function() {
+        $(this).toggleClass("year");
+    });
 });
-$(".close").click(function() {
-    $(".nav").removeClass("open");
-});
+
 
 
 
